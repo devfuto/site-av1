fb0aebb259a9e478f4c76e1009ded720b60c8061
Rafael Reis <132104445+devfuto@users.noreply.github.com>
2025-04-18 15:39:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+// Index
+function modoClaro() {
+  const container = document.querySelector(".conteudo-container");
+  const cabecalho = document.querySelector("#cabecalho");
+  const imagem = document.querySelector(".logo");
+  if (container && cabecalho && imagem) {
+    container.style.background = "#C19A6B";
+    cabecalho.style.background = "#94d182";
+    container.style.color = "#000";
+    imagem.src = "imagens/logo2.jpg";
+  }
+}
+
+function modoEscuro() {
+  const container = document.querySelector(".conteudo-container");
+  const cabecalho = document.querySelector("#cabecalho");
+  const imagem = document.querySelector(".logo");
+  if (container && cabecalho && imagem) {
+    container.style.background = "#492e1b";
+    cabecalho.style.background = "#366735";
+    container.style.color = "#FFF";
+    imagem.src = "imagens/logo.jpg";
+  }
+}
+
+// Pagina de formulário
+function modoClaro2() {
+  const container = document.querySelector(".conteudo-container2");
+  const cabecalho = document.querySelector("#cabecalho");
+  const imagem = document.querySelector(".logo");
+  if (container && cabecalho && imagem) {
+    container.style.background = "#C19A6B";
+    cabecalho.style.background = "#94d182";
+    container.style.color = "#000";
+    imagem.src = "imagens/logo2.jpg";
+  }
+}
+
+function modoEscuro2() {
+  const container = document.querySelector(".conteudo-container2");
+  const cabecalho = document.querySelector("#cabecalho");
+  const imagem = document.querySelector(".logo");
+  if (container && cabecalho && imagem) {
+    container.style.background = "#492e1b";
+    cabecalho.style.background = "#366735";
+    container.style.color = "#FFF";
+    imagem.src = "imagens/logo.jpg";
+  }
+}
+
+// Pagina Sobre
+
+function modoClaro3() {
+  const container = document.querySelector(".conteudo-container3");
+  const cabecalho = document.querySelector("#cabecalho");
+  const imagem = document.querySelector(".logo");
+  if (container && cabecalho && imagem) {
+    container.style.background = "#C19A6B";
+    cabecalho.style.background = "#94d182";
+    container.style.color = "#000";
+    imagem.src = "imagens/logo2.jpg";
+  }
+}
+
+function modoEscuro3() {
+  const container = document.querySelector(".conteudo-container3");
+  const cabecalho = document.querySelector("#cabecalho");
+  const imagem = document.querySelector(".logo");
+  if (container && cabecalho && imagem) {
+    container.style.background = "#492e1b";
+    cabecalho.style.background = "#366735";
+    container.style.color = "#FFF";
+    imagem.src = "imagens/logo.jpg";
+  }
+}
